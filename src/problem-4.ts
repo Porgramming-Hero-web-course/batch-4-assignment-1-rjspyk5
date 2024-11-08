b8c5148fd254b86772a6b37a7b5f49bcf4665e96
rjspyk5 <rjspyk5@gmail.com>
2024-11-09 01:00:20 +0600

    update problm four with typegurd

--- src/problem-4.ts
+++ src/problem-4.ts
@@ -3,18 +3,13 @@ type Rectangle={shape:"rectangle",width:number,height:number}
 
 const calculateShapeArea=(details:Circle | Rectangle):number=>{
 
-if (details?.shape==="circle") {
+if ("radius" in details) {
     return parseFloat((Math.PI*(details?.radius*details?.radius)).toFixed(2))
 }
-if (details?.shape==="rectangle") {
+if ("width" in details && "height" in details) {
     return parseFloat((details?.height*details?.width).toFixed(2));
 }
 return 0;
 }
 
-console.log(calculateShapeArea({
-    shape: "rectangle",
-    width: 4,
-    height: 6,
-  })
-);
+
